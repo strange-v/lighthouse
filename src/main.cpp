063ad904e559de7a53d852b2e297abf7d1083db9
strange_v <strange.rand@gmail.com>
2021-12-24 15:38:12 +0200

    Auto update time

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,8 @@
 void connectToWifi();
 void setupDateTime();
 bool isDateInRange(uint8_t hour, uint8_t minute, Alarm alarm);
+bool updateLight();
+bool setColor(RgbColor color, uint8_t ratio = 255);
 
 RgbColor black(0);
 RgbColor red(255, 0, 0);
@@ -19,12 +21,16 @@ RgbColor orange(255, 128, 0);
 
 NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod> led(Cfg::ledCount, Cfg::pinLed);
 RgbColor currentColor = black;
+uint64_t lastTimeSync = 0;
 
 Alarm alarms[] = 
 {
-  {true, 6, 0, 20, 30, green, 255},
-  {true, 20, 30, 20, 55, orange, 255},
-  {true, 20, 55, 6, 0, red, 100},
+  {true, 6, 0, 12, 30, green, 150},
+  {true, 12, 30, 12, 50, orange, 255},
+  {true, 12, 50, 15, 0, red, 255},
+  {true, 15, 0, 20, 30, green, 255},
+  {true, 20, 30, 20, 50, orange, 200},
+  {true, 20, 50, 6, 0, red, 100},
 };
 
 void setup()
@@ -47,25 +53,44 @@ void loop()
 {
   ArduinoOTA.handle();
 
+  uint64_t ms = millis();
+  if (ms - lastTimeSync >= Cfg::syncTimeInterval || ms < lastTimeSync)
+  {
+    if (WiFi.status() == WL_CONNECTED)
+      setupDateTime();
+  }
+    
+  if (DateTime.isTimeValid())
+    updateLight();
+  else
+    setColor(black);  
+}
+
+bool updateLight()
+{
   DateTimeParts dt = DateTime.getParts();
   for (uint8_t i = 0; i < sizeof(alarms)/sizeof(alarms[0]); i++)
+  {
+    Alarm alarm = alarms[i];
+    if (isDateInRange(dt.getHours(), dt.getMinutes(), alarm))
     {
-      Alarm alarm = alarms[i];
-      if (isDateInRange(dt.getHours(), dt.getMinutes(), alarm))
-      {
-        if (currentColor != alarm.color)
-        {
-          currentColor = alarm.color.Dim(alarm.colorRatio);
-          led.SetPixelColor(0, currentColor);
-          led.Show();
-        }
-      }
+      setColor(alarm.color, alarm.colorRatio);
     }
+  }
+}
+
+bool setColor(RgbColor color, uint8_t ratio)
+{
+  if (currentColor != color)
+  {
+    currentColor = color.Dim(ratio);
+    led.SetPixelColor(0, currentColor);
+    led.Show();
+  }
 }
 
 void connectToWifi()
 {
-  
   WiFi.begin(CfgSecure::wifiSsid, CfgSecure::wifiPassword);
   
   Serial.print("Connecting to WiFi");
@@ -80,16 +105,17 @@ void connectToWifi()
 
 void setupDateTime()
 {
-  DateTime.setServer("time.pool.aliyun.com");
+  DateTime.setServer("pool.ntp.org");
   DateTime.setTimeZone(TZ_Europe_Kiev);
   DateTime.begin();
 
   if (!DateTime.isTimeValid())
   {
     Serial.println("Failed to get time from server.");
+    lastTimeSync = 0;
     return;
   }
-
+  lastTimeSync = millis();
   Serial.printf("Date Now is %s\n", DateTime.toISOString().c_str());
 }
 
