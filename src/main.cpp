051aeda7d6039db23571316d45e06350bf8db54f
strange_v <strange.rand@gmail.com>
2023-06-24 11:06:40 +0300

    Use native time, set hostname correctly

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,7 @@
 #include <Arduino.h>
 #include <WiFi.h>
 #include <ArduinoOTA.h>
-#include "ESPDateTime.h"
-#include "DateTimeTZ.h"
+#include <Time.h>
 #include "NeoPixelBus.h"
 #include "Cfg.h"
 #include "CfgSecure.h"
@@ -10,13 +9,15 @@
 
 void connectToWifi();
 void setupDateTime();
+bool isTimeSynchronized();
 bool isDateInRange(uint8_t hour, uint8_t minute, Alarm alarm);
-bool updateLight();
-bool setColor(RgbColor color, uint8_t ratio = 255);
+void updateLight();
+void setColor(RgbColor color, uint8_t ratio = 255);
 
 RgbColor black(0);
 RgbColor red(255, 0, 0);
 RgbColor green(0, 255, 0);
+RgbColor blue(0, 0, 255);
 RgbColor orange(255, 128, 0);
 
 NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod> led(Cfg::ledCount, Cfg::pinLed);
@@ -25,12 +26,12 @@ uint64_t lastTimeSync = 0;
 
 Alarm alarms[] = 
 {
-  {true, 6, 0, 12, 30, green, 150},
-  {true, 12, 30, 12, 50, orange, 255},
-  {true, 12, 50, 15, 0, red, 255},
-  {true, 15, 0, 20, 30, green, 255},
-  {true, 20, 30, 20, 50, orange, 200},
-  {true, 20, 50, 6, 0, red, 100},
+  {true, 6, 45, 12, 30, green, 100},
+  {true, 12, 30, 12, 50, blue, 255},
+  {true, 12, 50, 14, 40, red, 255},
+  {true, 14, 40, 20, 30, green, 255},
+  {true, 20, 30, 20, 50, blue, 200},
+  {true, 20, 50, 6, 30, red, 100},
 };
 
 void setup()
@@ -41,7 +42,6 @@ void setup()
   led.Begin();
   led.Show();
 
-  WiFi.mode(WIFI_STA);
   connectToWifi();
   setupDateTime();
 
@@ -60,26 +60,31 @@ void loop()
       setupDateTime();
   }
     
-  if (DateTime.isTimeValid())
+  if (isTimeSynchronized())
     updateLight();
   else
     setColor(black);  
 }
 
-bool updateLight()
+void updateLight()
 {
-  DateTimeParts dt = DateTime.getParts();
+  time_t now;
+  tm local;
+
+  time(&now);
+  localtime_r(&now, &local);
+
   for (uint8_t i = 0; i < sizeof(alarms)/sizeof(alarms[0]); i++)
   {
     Alarm alarm = alarms[i];
-    if (isDateInRange(dt.getHours(), dt.getMinutes(), alarm))
+    if (isDateInRange(local.tm_hour, local.tm_min, alarm))
     {
       setColor(alarm.color, alarm.colorRatio);
     }
   }
 }
 
-bool setColor(RgbColor color, uint8_t ratio)
+void setColor(RgbColor color, uint8_t ratio)
 {
   if (currentColor != color)
   {
@@ -91,6 +96,9 @@ bool setColor(RgbColor color, uint8_t ratio)
 
 void connectToWifi()
 {
+  WiFi.mode(WIFI_MODE_NULL);
+  WiFi.setHostname("lighthouse");
+  WiFi.mode(WIFI_STA);
   WiFi.begin(CfgSecure::wifiSsid, CfgSecure::wifiPassword);
   
   Serial.print("Connecting to WiFi");
@@ -105,18 +113,22 @@ void connectToWifi()
 
 void setupDateTime()
 {
-  DateTime.setServer("pool.ntp.org");
-  DateTime.setTimeZone(TZ_Europe_Kiev);
-  DateTime.begin();
+  configTime(0, 0, "ua.pool.ntp.org");
+  isTimeSynchronized();
 
-  if (!DateTime.isTimeValid())
-  {
-    Serial.println("Failed to get time from server.");
-    lastTimeSync = 0;
-    return;
-  }
-  lastTimeSync = millis();
-  Serial.printf("Date Now is %s\n", DateTime.toISOString().c_str());
+  setenv("TZ", "WET0WEST,M3.5.0/1,M10.5.0", 5);
+  tzset();
+}
+
+bool isTimeSynchronized()
+{
+    time_t now;
+    time(&now);
+
+    tm local;
+    localtime_r(&now, &local);
+
+    return local.tm_year > (2016 - 1900);
 }
 
 bool isDateInRange(uint8_t hour, uint8_t minute, Alarm alarm)
